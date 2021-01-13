681acbbb3ac3da3f9efd72827ee2a0e8e68a85f5
wayne <iga33087@gmail.com>
2021-01-13 12:25:54 +0800

    完善首頁

---
+++ src/components/QRBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="QRbox">
+    <img class="QRboxImg" src="@/assets/img/QR.jpg">
+    <div class="QRboxInfo">
+      <img class="QRboxInfoImg" src="@/assets/img/download1.png">
+      <img class="QRboxInfoImg" src="@/assets/img/download2.png">
+      <div class="QRboxInfoText">
+       唯一全民娱乐棋牌<br>娱乐久久 快乐皆有
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,67 @@
 <template>
   <div class="home">
     <Header :icon="'/img/homeLogo.png'" />
-    <el-carousel class="homeCarousel" trigger="click" height="566px">
+    <el-carousel class="homeCarousel" trigger="click">
+      <QRBox/>
       <el-carousel-item v-for="item in 4" :key="item">
-        <div class="homeCarouselItem">{{item}}</div>
+        <div class="homeCarouselItem"><img class="homeCarouselItemImg" src="@/assets/img/imgBox/imgBox1.png"></div>
+        <div class="homeCarouselItem"><img class="homeCarouselItemImg" src="@/assets/img/imgBox/imgBox2.png"></div>
       </el-carousel-item>
     </el-carousel>
     <div class="content">
       <div class="homeBox">
         <div class="homeBoxTitle">公平绿色竞技 棋牌APP第一选择</div>
         <div class="homeBoxContent">
-          <router-link :to="{path:'/Page1'}">
-            <div class="homeBoxContentItemType1">
-              <img class="homeBoxContentItemType1Img" src="img/gameLogo1.png">
-              <div class="homeBoxContentItemType1Text">斗智斗牌<br>烧脑战争</div>
-            </div>
+          <router-link class="homeBoxContentItemType1" :to="{path:'/Page1'}">
+            <img class="homeBoxContentItemType1Img" src="img/gameLogo1.png">
+            <div class="homeBoxContentItemType1Text">斗智斗牌<br>烧脑战争</div>
           </router-link>
-          <div class="homeBoxContentItemType1">
+          <router-link class="homeBoxContentItemType1" :to="{path:'/Page1'}">
             <img class="homeBoxContentItemType1Img" src="img/gameLogo5.png">
             <div class="homeBoxContentItemType1Text">全民接战<br>极速刺激</div>
-          </div>
-          <div class="homeBoxContentItemType1">
+          </router-link>
+          <router-link class="homeBoxContentItemType1" :to="{path:'/Page1'}">
             <img class="homeBoxContentItemType1Img" src="img/gameLogo4.png">
             <div class="homeBoxContentItemType1Text">运气拼点<br>由底翻身</div>
-          </div>
-          <div class="homeBoxContentItemType1">
+          </router-link>
+          <router-link class="homeBoxContentItemType1" :to="{path:'/Page1'}">
             <img class="homeBoxContentItemType1Img" src="img/gameLogo3.png">
             <div class="homeBoxContentItemType1Text">公牌作主<br>人面赢家</div>
-          </div>
-          <div class="homeBoxContentItemType1">
+          </router-link>
+          <router-link class="homeBoxContentItemType1" :to="{path:'/Page1'}">
             <img class="homeBoxContentItemType1Img" src="img/gameLogo2.png">
             <div class="homeBoxContentItemType1Text">中国德州<br>公平呐喊</div>
+          </router-link>
+        </div>
+      </div>
+      <div class="homeBox">
+        <div class="homeBoxTitle">纯绿色娱乐</div>
+        <div class="homeBoxContent2">
+          <div class="homeBoxContentItemType2">
+            24小时技术客服支持<br>秒问秒回 火速处理
+          </div>
+          <div class="homeBoxContentItemType2">
+            专业蓝光连线技术<br>5G流畅
+          </div>
+          <div class="homeBoxContentItemType2">
+            BAT级服务器终端<br>客户连结
+          </div>
+          <div class="homeBoxContentItemType2">
+            绿色公平竞技<br>健康扑克游戏
           </div>
         </div>
       </div>
       <div class="homeBox">
         <div class="homeBoxTitle">享受反客为主的乐趣</div>
-        <div class="homeBoxContent"></div>
-      </div>
-      <div class="homeBox">
-        <div class="homeBoxTitle">纯绿色娱乐</div>
-        <div class="homeBoxContent"></div>
+        <div class="homeBoxContent homeBoxContent3">
+          <div class="homeBoxContentVideo"></div>
+          <img class="homeBoxContentQR" src="@/assets/img/QR.jpg">
+          <div class="homeBoxContentInfo">
+            <img class="homeBoxContentInfoImg" src="@/assets/img/download1.png">
+            <img class="homeBoxContentInfoImg" src="@/assets/img/download2.png">
+            <div class="homeBoxContentInfoText">立即下载<br>畅游扑克世界</div>
+          </div>
+        </div>
       </div>
     </div>
     <Footer/>
@@ -51,10 +72,11 @@
 // @ is an alias to /src
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+import QRBox from '@/components/QRBox.vue'
 
 export default {
   components: {
-    Header,Footer
+    Header,Footer,QRBox
   },
   created() {
     document.body.style.backgroundImage="url('img/homeBg.png')"
