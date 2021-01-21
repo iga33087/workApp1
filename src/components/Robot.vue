b97fc2e4e84c509fc094951e3f3d9e685f273c84
wayne <iga33087@gmail.com>
2021-01-21 19:18:38 +0800

    修改robot

--- src/components/Robot.vue
+++ src/components/Robot.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="robot" @click="open" v-if="show">
+    <div class="robotBlack" @click.stop="open" v-if="showBox">
+      <div class="robotBlackBox">
+        <div class="robotBlackBoxTitle">
+          <img class="robotBlackBoxTitleImg" src="@/assets/img/logo/homeLogo.png">
+          <div>請掃QR Code</div>
+        </div>
+        <div class="robotBlackBoxContent">
+          <img class="robotBlackBoxContentImg" src="@/assets/img/客服wechat.jpg">
+          <div class="robotBlackBoxContentText">微信号:jlhuyu8888</div>
+        </div>
+      </div>
+    </div>
     <i class="el-icon-close robotX" @click.stop="show=!show"/>
-    <img class="robotImg" src="@/assets/img/robot.png">
+      <img class="robotImg" src="@/assets/img/robot.png">
   </div>
 </template>
 
@@ -9,12 +21,14 @@
 export default {
   data() {
     return {
-      show:true
+      show:true,
+      showBox:false
     }
   },
   methods: {
     open() {
-      window.open("https://u.wechat.com/IGrEG2cOi2L3Rh1f0IhIeIY");
+      this.showBox=!this.showBox
+      //window.open("https://u.wechat.com/IGrEG2cOi2L3Rh1f0IhIeIY");
     }
   }
 }
