ade14dc8785788fec5b1859b1746731c73d27204
wayne <iga33087@gmail.com>
2021-01-26 15:13:28 +0800

    完善介紹

---
+++ src/components/InfoVideoLine.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="infoVideoLine">
+    <div class="infoVideoLine" :style="{'color':fontColor}">{{text}}</div>
+    <div class="infoVideoShadow" :style="{'-webkit-text-stroke': '10px '+borderColor}">{{text}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {},
+    fontColor: {
+      default:"#fff"
+    },
+    borderColor: {
+      default:"#f00"
+    }
+  }
+}
+</script>
---
+++ src/components/RuleItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <li class="ruleItem" :class="{'typeNone':typeNone}">
+    <div class="ruleItemContent">{{data.content}}</div>
+    <div class="ruleItemChilden" v-if="data.childen.length">
+      <ul>
+        <RuleItem v-for="(item,index) in data.childen" :data="item" :key="index" />
+      </ul>
+    </div>
+  </li>
+</template>
+
+<script>
+import RuleItem from '@/components/RuleItem.vue'
+export default {
+  name:"RuleItem",
+  props: {
+    data: {},
+    typeNone: {
+      default:false
+    }
+  },
+  components: {RuleItem}
+}
+</script>
+
+<style scoped>
+  .typeNone {
+    list-style-type:none;
+  }
+</style>
